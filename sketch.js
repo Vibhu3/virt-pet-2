648bf0f29cbe93a7d7e296d29babe5b21ad27049
Vibhu3 <65866042+Vibhu3@users.noreply.github.com>
2020-08-15 16:09:36 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var database ,dog,dogImg,happydogImg
+var position
+//var form
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
+//Create variables here
+
+function preload()
+
+{
+  dogImg = loadImage("images/dogImg.png")
+  happydogImg = loadImage("images/dogImg1.png")
+
+	//load images here
+}
+
+function setup() {
+	createCanvas(1000, 500);
+  database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogImg)
+  dog.scale=0.2
+  
+ 
+
+   dog = database.ref('Food');
+  dog.on("value", readPosition, showError);
+feed = createButton("FEED DRAGO")
+feed.position(500,15)
+feed.mousePressed(feedDog)
+add = createButton("ADD FOOD")
+add.position(400,15)
+add.mousePressed(AddFood)
+
+} 
+
+
+
+function draw(){
+ { background(46,139,87);
+ foodobject.display()
+ 
+ }
+ drawSprites();
+  
+  fill(255,255,254);
+ textSize(15);
+
+  // text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+ 
+  //add styles here
+drawSprites();
+}
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
+  console.log(position.x);
+  
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
+
+function writePosition(x){
+  if(x>0){
+    x=x-1
+  }
+  else{
+    x=0
+  }
+  database.ref('/').set({
+    'Food': x
+  })
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
+}
+
+)
+}
+function feedDog(){
+
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
+}
